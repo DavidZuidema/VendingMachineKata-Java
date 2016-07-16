c8d7c55519442d3bd5308d4859b0248b1e2daff8
David Zuidema <dczuidema@gmail.com>
2016-07-16 14:04:14 -0400

    Refactor VendingMachineTest to reduce duplication and increase semantics

--- test/com/acme/vending/VendingMachineTest.java
+++ test/com/acme/vending/VendingMachineTest.java
@@ -1,11 +1,9 @@
 package com.acme.vending;
 
-import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class VendingMachineTest {
@@ -25,48 +23,62 @@ public class VendingMachineTest {
 
 	@Test
 	public void whenNoCoinsHaveBeenInserted_itDisplaysTheDefaultMessage() {
-		checkThatMachineDisplaysMessage(DEFAULT_VENDING_MACHINE_MESSAGE);
+		displaysMessage(DEFAULT_VENDING_MACHINE_MESSAGE);
 	}
 	
 	@Test
 	public void whenAnInvalidCoinHasBeenInserted_itDisplaysTheDefaultMessage() {
-		vendingMachine.insertCoin(INVALID_COIN);
-		checkThatMachineDisplaysMessage(DEFAULT_VENDING_MACHINE_MESSAGE);
+		insert(INVALID_COIN);
+		displaysMessage(DEFAULT_VENDING_MACHINE_MESSAGE);
 	}
 	
 	@Test
 	public void whenOneInvalidCoinHasBeenInserted_itAddsThatCoinToTheCoinReturn() {
-		vendingMachine.insertCoin(INVALID_COIN);
-		assertThat(vendingMachine.getCoinsInCoinReturn(), hasItem(INVALID_COIN));
+		insert(INVALID_COIN);
+		coinReturnContains(INVALID_COIN);
 	}
 	
 	@Test
-	public void whenTwoInvalidCoinsHaveBeenInserted_itAddsBothCoinToTheCoinReturn() {
-		vendingMachine.insertCoin(INVALID_COIN);
-		vendingMachine.insertCoin(ANOTHER_INVALID_COIN);
-		assertEquals(2, vendingMachine.getCoinsInCoinReturn().size());
-		assertThat(vendingMachine.getCoinsInCoinReturn(), hasItems(INVALID_COIN, ANOTHER_INVALID_COIN));
+	public void whenTwoInvalidCoinsHaveBeenInserted_itAddsBothCoinsToTheCoinReturn() {
+		insert(INVALID_COIN);
+		insert(ANOTHER_INVALID_COIN);
+		coinReturnContains(INVALID_COIN, ANOTHER_INVALID_COIN);
 	}
 	
 	@Test
 	public void whenANickelIsInserted_itIsNotAddedToTheCoinReturn() {
-		vendingMachine.insertCoin(CoinType.NICKEL);
-		assertEquals(0, vendingMachine.getCoinsInCoinReturn().size());
+		insert(CoinType.NICKEL);
+		coinReturnIsEmpty();
 	}
 	
 	@Test
 	public void whenANickelIsInserted_itDisplaysTheTotalDeposited() {
-		vendingMachine.insertCoin(CoinType.NICKEL);
-		checkThatMachineDisplaysMessage("$0.05");
+		insert(CoinType.NICKEL);
+		displaysMessage("$0.05");
 	}
 	
 	@Test
 	public void whenADimeIsInserted_itDisplaysTheTotalDeposited() {
-		vendingMachine.insertCoin(CoinType.DIME);
-		checkThatMachineDisplaysMessage("$0.10");
+		insert(CoinType.DIME);
+		displaysMessage("$0.10");
 	}
 	
-	private void checkThatMachineDisplaysMessage(String message) {
+
+	
+	private void insert(CoinType coin) {
+		vendingMachine.insertCoin(coin);
+	}
+	
+	private void displaysMessage(String message) {
 		assertEquals(message, vendingMachine.getDisplay());
+	}
+
+	private void coinReturnContains(CoinType...coinTypes) {
+		assertEquals(coinTypes.length, vendingMachine.getCoinsInCoinReturn().size());
+		assertThat(vendingMachine.getCoinsInCoinReturn(), hasItems(coinTypes));
+	}
+	
+	private void coinReturnIsEmpty() {
+		assertEquals(0, vendingMachine.getCoinsInCoinReturn().size());
 	}
 }
