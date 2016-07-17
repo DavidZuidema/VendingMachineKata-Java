f4ca5d599aec7e5b968919106f927dec3e5684dc
David Zuidema <dczuidema@gmail.com>
2016-07-17 15:05:11 -0400

    Test CoinChanger always makes change in largest denominations first

--- test/com/acme/vending/CoinChangerTest.java
+++ test/com/acme/vending/CoinChangerTest.java
@@ -1,12 +1,20 @@
 package com.acme.vending;
 
 import static org.hamcrest.CoreMatchers.hasItem;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class CoinChangerTest {
+
+	private static final CoinType NICKEL = CoinType.NICKEL;
+	private static final CoinType DIME = CoinType.DIME;
+	private static final CoinType QUARTER = CoinType.QUARTER;
 
 	private CoinChanger coinChanger;
 
@@ -17,9 +25,40 @@ public class CoinChangerTest {
 	
 	@Test
 	public void whenMakingChangeForSingleCoins_returnsSameCoin() {
-		assertThat(coinChanger.makeChange(5), hasItem(CoinType.NICKEL));
-		assertThat(coinChanger.makeChange(10), hasItem(CoinType.DIME));
-		assertThat(coinChanger.makeChange(25), hasItem(CoinType.QUARTER));
+		assertThat(coinChanger.makeChange(5), hasItem(NICKEL));
+		assertThat(coinChanger.makeChange(10), hasItem(DIME));
+		assertThat(coinChanger.makeChange(25), hasItem(QUARTER));
 	}
 	
+	@Test
+	public void whenMakingChangeForFiftyCents_returnsLargestDenominationsAvailable() {
+		Collection<CoinType> coins = coinChanger.makeChange(50);
+		assertEquals(2, coins.size());
+		assertThat(coins, hasItem(QUARTER));
+		assertThat(coins, not(hasItems(DIME, NICKEL)));
+	}
+	
+	@Test
+	public void whenMakingChangeForTwentyCents_returnsLargestDenominationsAvailable() {
+		Collection<CoinType> coins = coinChanger.makeChange(20);
+		assertEquals(2, coins.size());
+		assertThat(coins, hasItem(DIME));
+		assertThat(coins, not(hasItems(QUARTER, NICKEL)));
+	}
+	
+	@Test
+	public void whenMakingChangeForFifteenCents_returnsMultipleDenominations() {
+		Collection<CoinType> coins = coinChanger.makeChange(15);
+		assertEquals(2, coins.size());
+		assertThat(coins, hasItems(DIME, NICKEL));
+		assertThat(coins, not(hasItems(QUARTER)));
+	}
+	
+	@Test
+	public void whenMakingChangeForSeventyCents_returnsMultipleDenominations() {
+		Collection<CoinType> coins = coinChanger.makeChange(70);
+		assertEquals(4, coins.size());
+		assertThat(coins, hasItems(DIME, QUARTER));
+		assertThat(coins, not(hasItems(NICKEL)));
+	}
 }
