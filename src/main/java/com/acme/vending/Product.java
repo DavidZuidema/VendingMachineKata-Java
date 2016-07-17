3e277562c14da02e255f3c41867cbbabf24dec77
David Zuidema <dczuidema@gmail.com>
2016-07-17 17:32:51 -0400

    Code products can identify when they are in stock

--- src/main/java/com/acme/vending/Product.java
+++ src/main/java/com/acme/vending/Product.java
@@ -4,10 +4,12 @@ public class Product {
 	
 	private String name = "";
 	private int priceInCents = 0;
+	private int inventoryCount = 0;
 	
 	public Product (String name, int priceInCents, int inventoryCount) {
 		this.name = name;
 		this.priceInCents = priceInCents;
+		this.inventoryCount  = inventoryCount;
 	}
 
 	public int getPriceInCents() {
@@ -19,6 +21,6 @@ public class Product {
 	}
 
 	public boolean isInStock() {
-		return false;
+		return inventoryCount > 0;
 	}
 }
