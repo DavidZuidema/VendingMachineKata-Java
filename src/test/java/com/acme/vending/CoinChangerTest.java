4359a0ea7791cd24c1cdb6219adb98ec63192ab2
David Zuidema <dczuidema@gmail.com>
2016-07-17 16:14:17 -0400

    Refactor CoinChanger test to reduce duplication and increase semantics

--- src/test/java/com/acme/vending/CoinChangerTest.java
+++ src/test/java/com/acme/vending/CoinChangerTest.java
@@ -1,18 +1,10 @@
 package com.acme.vending;
 
-import static org.hamcrest.CoreMatchers.hasItem;
-import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.contains;
 import static org.junit.Assert.*;
 
-import java.util.Collection;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.internal.matchers.Contains;
-
-import com.google.common.collect.Lists;
 
 public class CoinChangerTest {
 
@@ -29,38 +21,24 @@ public class CoinChangerTest {
 	
 	@Test
 	public void whenMakingChangeForSingleCoins_returnsSameCoin() {
-		assertThat(coinChanger.makeChange(5), hasItem(NICKEL));
-		assertThat(coinChanger.makeChange(10), hasItem(DIME));
-		assertThat(coinChanger.makeChange(25), hasItem(QUARTER));
+		makeChangeForAmountReturnCoins(5, NICKEL);
+		makeChangeForAmountReturnCoins(10, DIME);
+		makeChangeForAmountReturnCoins(25, QUARTER);
 	}
 	
 	@Test
-	public void whenMakingChangeForFiftyCents_returnsLargestDenominationsAvailable() {
+	public void whenMakingChange_returnsLargestDenominationsAvailable() {
 		makeChangeForAmountReturnCoins(50, QUARTER, QUARTER);
+		makeChangeForAmountReturnCoins(20, DIME, DIME);
 	}
 	
 	@Test
-	public void whenMakingChangeForTwentyCents_returnsLargestDenominationsAvailable() {
-		makeChangeForAmountReturnCoins(20, DIME, DIME);
+	public void whenMakingChange_returnsMultipleDenominations() {
+		makeChangeForAmountReturnCoins(15, DIME, NICKEL);
+		makeChangeForAmountReturnCoins(70, QUARTER, QUARTER, DIME, DIME);
 	}
 	
 	private void makeChangeForAmountReturnCoins(int amountInCents, CoinType...coinsReturned) {
 		assertThat(coinChanger.makeChange(amountInCents), contains(coinsReturned));
-	}
-	
-	@Test
-	public void whenMakingChangeForFifteenCents_returnsMultipleDenominations() {
-		Collection<CoinType> coins = coinChanger.makeChange(15);
-		assertEquals(2, coins.size());
-		assertThat(coins, hasItems(DIME, NICKEL));
-		assertThat(coins, not(hasItems(QUARTER)));
-	}
-	
-	@Test
-	public void whenMakingChangeForSeventyCents_returnsMultipleDenominations() {
-		Collection<CoinType> coins = coinChanger.makeChange(70);
-		assertEquals(4, coins.size());
-		assertThat(coins, hasItems(DIME, QUARTER));
-		assertThat(coins, not(hasItems(NICKEL)));
 	}
 }
